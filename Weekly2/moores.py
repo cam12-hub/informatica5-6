59d38299a9af5c302a744ae0f62c3a36749e21cf
cam12-hub <camilaolivas008@gmail.com>
2026-08-20 14:43:01 +0000

    this class was eassyyyyy

--- Weekly2/moores.py
+++ Weekly2/moores.py
@@ -1,8 +1,8 @@
 def main():
-    transitors = 17800000000
+    transistors = 17800000000
     years = int(input("How many years into the future?"))
-    transitors *= round(2** (years/2))
-    print(f"{transitors:,}")
+    transistors *= round(2** (years/2))
+    print(f"{transistors:,}")
 
 if __name__ == "__main__":
     main()
