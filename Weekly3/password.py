093681baf5d7b2fb394d9462974f1d8c7d964668
cam12-hub <camilaolivas008@gmail.com>
2026-08-24 14:57:12 +0000

    Super Secret Aura Farming Battle

---
+++ Weekly3/password.py
@@ -0,0 +1,11 @@
+def main():
+
+    password = 1234
+    attempt = input("Type correct password: ")
+    if attempt == password:
+        print("Welcome to the Super Secret Aura Farming Battle/FBI V1")
+
+    print("You lost a 1000 aura points, please enter password you doofus")
+
+if __name__ == "__main__":
+    main()
