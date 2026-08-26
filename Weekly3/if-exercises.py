3034e596357e73c0d4aab677b5962e049a2acec6
cam12-hub <camilaolivas008@gmail.com>
2026-08-26 18:10:55 +0000

    i gotta finish this thiiiing

---
+++ Weekly3/if-exercises.py
@@ -0,0 +1,12 @@
+def main():
+    n1 = float(input("Type a number: "))
+    operation  = input("Type an operation: ")
+    n2 = float(input("Type another number: "))
+    if operation == "+":
+        print(n1 + n2)
+    elif operation == "*":
+
+ #string.split " "
+
+if __name__ == "__main__":
+    main()
