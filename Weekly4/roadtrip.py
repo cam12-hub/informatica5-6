23f8716643e8f57c19cb7e4a6f36a98e075fb249
cam12-hub <camilaolivas008@gmail.com>
2026-09-02 18:06:58 +0000

    el profe hace trampa, that's not fair.

--- Weekly4/roadtrip.py
+++ Weekly4/roadtrip.py
@@ -1,12 +1,12 @@
 def main():
     answer = ""
-
+    followup = ""
     while answer != "Yes!":
         answer = input("Are we there yet? ").title().strip()
         if answer == "Yes":
             followup = input("Really? ").title().strip()
-            if followup == "Yes":
-                break
+        if followup == "Yes":
+            break
 
     print("We just arrived!")
 
