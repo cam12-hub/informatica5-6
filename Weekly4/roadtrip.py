d6e49c8cc8ae01e740db148c68e141d25af1f4bc
cam12-hub <camilaolivas008@gmail.com>
2026-09-02 18:00:48 +0000

    por andar de preguntones le agregamos más a una actividad que iba a ser sencilla

--- Weekly4/roadtrip.py
+++ Weekly4/roadtrip.py
@@ -3,6 +3,11 @@ def main():
 
     while answer != "Yes!":
         answer = input("Are we there yet? ").title().strip()
+        if answer == "Yes":
+            followup = input("Really? ").title().strip()
+            if followup == "Yes":
+                break
+
     print("We just arrived!")
 
 if __name__ == "__main__":
