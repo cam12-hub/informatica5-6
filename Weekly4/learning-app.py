3476ed3aa6827b6f73a3a96d832083ca516136c8
cam12-hub <camilaolivas008@gmail.com>
2026-09-04 15:49:22 +0000

    I don't like brilliant from the hoood

---
+++ Weekly4/learning-app.py
@@ -0,0 +1,29 @@
+import random
+def main():
+
+    print("Welcome to Brilliant from the hood!")
+    addition = int("" + "")
+    answer = ("")
+
+    while answer != addition:
+        num1 = random.randint(10, 99)
+        num2 = random.randint(10, 99)
+        print(f"What is {num1} + {num2}?")
+        
+        if  answer > addition:
+                print("Incorrect")
+                print(f"The answer was {addition}")
+        elif  answer > addition:
+                print("Incorrect")
+                print(f"The answer was {addition}")
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
