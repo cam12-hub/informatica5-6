833cdfc7ae207149f53b2e207198ff491afa5821
cam12-hub <camilaolivas008@gmail.com>
2026-09-23 18:05:22 +0000

    this thing was fuerteeee

--- Weekly6/tables.py
+++ Weekly6/tables.py
@@ -1,11 +1,16 @@
 def main():
-    number = int(input("Enter a number (1-10): "))
-    command = "exit"
-    print(f"Here is the {number} times table")
-    for i in range (1,11):
-        result = i * number
-        print(f"{i} * {number} = {result}" )
-
+    while True:
+        number = int(input("Enter a number (1-10): "))
+        if number <= 10:
+            print(f"Here is the {number} times table")
+            for i in range (1,11):
+                result = i * number
+                print(f"{i} * {number} = {result}" )
+        command = input("Do you wanna keep learning? ").lower().strip()
+        if command == "yes":
+            continue
+        else:
+            break
 
 
 
